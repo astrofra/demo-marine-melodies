1a801c0d9e3aa845deec3a6ee2acce61012642bd
astrofra <astrofra@gmail.com>
2023-03-23 20:46:38 +0100

    XUL edition WIP.

---
+++ src/read-serial.py
@@ -0,0 +1,27 @@
+import asyncio
+import serial_asyncio
+
+async def read_serial_data(serial_port):
+    while True:
+        line = await serial_port.readline()
+        print(f'Received: {line.decode().strip()}')
+
+async def main():
+    # Replace 'COM3' with the serial port name on your system
+    port = 'COM3'
+    baudrate = 115200
+
+    # Create a connection to the serial port
+    serial_port = await serial_asyncio.open_serial_connection(url=port, baudrate=baudrate)
+
+    # Start the async task to read data from the serial port
+    read_task = asyncio.create_task(read_serial_data(serial_port))
+
+    # You can add other async tasks here
+    # e.g., asyncio.create_task(some_other_async_function())
+
+    # Wait for the read task to complete (in this case, it will never complete)
+    await read_task
+
+if __name__ == '__main__':
+    asyncio.run(main())
